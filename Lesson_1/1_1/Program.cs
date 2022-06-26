c2c89deea84c662fc70a955a13ba2c772db3d7df
alla <fi-alka@yandex.ru>
2022-06-26 21:00:26 +0300

    Task 1_1

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, people!");
+﻿//string num_str = Console.ReadLine();
+//int num = int.Parse(num_str);
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
