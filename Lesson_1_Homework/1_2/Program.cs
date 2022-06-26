82935c043ee9d85299f69b20321541bf4fddb665
alla <fi-alka@yandex.ru>
2022-06-26 22:53:58 +0300

    Дом.задание №2

---
+++ Lesson_1_Homework/1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num2 > max) max = num3;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
