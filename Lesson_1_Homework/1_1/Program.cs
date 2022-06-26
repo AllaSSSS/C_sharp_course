1daaa9c570da0d5c9a8b355d56056502b38d911a
alla <fi-alka@yandex.ru>
2022-06-26 22:40:04 +0300

    Дом.задание №1

---
+++ Lesson_1_Homework/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write("большее число =");
+    Console.WriteLine(num1);
+    Console.Write("меньшее число =");
+    Console.WriteLine(num2);
+}
+
+else
+{
+    Console.Write("большее число =");
+    Console.WriteLine(num2);
+    Console.Write("меньшее число =");
+    Console.WriteLine(num1);
+}
