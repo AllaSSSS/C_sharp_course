e0f737742fde01785ecc6c16c8c9eba2add62ce1
alla <fi-alka@yandex.ru>
2022-06-26 23:13:16 +0300

    Дом.задание №4

---
+++ Lesson_1_Homework/1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = int.Parse(Console.ReadLine());
+int count = 2;
+
+while (count <= num)
+{
+    if (count > 2) Console.Write(", ");
+    
+    Console.Write(count);
+    count += 2;
+}
+Console.WriteLine();
