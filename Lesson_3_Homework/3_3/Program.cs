7b137eaabee81981962b78620e8826c5eea90b3b
alla <fi-alka@yandex.ru>
2022-07-03 19:51:56 +0300

    Д/з №3.3

---
+++ Lesson_3_Homework/3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+
+void Cube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.Write($"{Math.Pow(count, 3)} ");
+        count++;
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Cube(num);
