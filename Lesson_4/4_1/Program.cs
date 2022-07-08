ccfdd9fea295b66cb428b026c3bb057fad5d2c22
alla <fi-alka@yandex.ru>
2022-07-08 10:37:41 +0300

    Задача 4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход число А и выдает сумму чисел от 1 до А.
+
+int SumNum(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++) result += i;
+    return result;
+}
+
+Console.WriteLine(SumNum(7));
