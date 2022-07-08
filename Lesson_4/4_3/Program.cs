c4db4cdcfe145191ee03592602b75b9822468a02
alla <fi-alka@yandex.ru>
2022-07-08 11:03:20 +0300

    Задача 4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает число N и выдает произведение от 1 до N
+int Product(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++) result *= i;
+    return result;
+}
+
+Console.WriteLine(Product(4));
