5b842615b0152e362f7e448123ec39e5844d2478
alla <fi-alka@yandex.ru>
2022-07-09 00:47:59 +0300

    Д

---
+++ Lesson_4_Homework/4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Цикл принимает на вход два числа А и В и возводит число А в степень В
+
+int Power (int num1, int num2)
+{
+    int result = num1;
+    for(int i = 1; i < num2; i++)
+    {
+        result *= num1;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите два числа: ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int result = Power(num1, num2);
+Console.WriteLine(result);
