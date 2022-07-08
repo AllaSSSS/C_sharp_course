291a4b83327956ccb39ba9a37d5c7768823eb466
alla <fi-alka@yandex.ru>
2022-07-09 01:09:54 +0300

    Д/з 2

---
+++ Lesson_4_Homework/4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает на вход число и выдает сумму цифр в числе
+
+int SumOfDigits(int num)
+{
+    int result = 0; 
+    while(num >= 1)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int result = SumOfDigits(num);
+Console.WriteLine(result);
