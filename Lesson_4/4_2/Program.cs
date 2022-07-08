42db0a3dd6dbbc6a4c18754d9f60aae64774bdfb
alla <fi-alka@yandex.ru>
2022-07-08 10:47:34 +0300

    Задача 4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает на вход число и выдает количество цифр в числе
+
+int DigitCount(int num)
+{
+    string result = num.ToString();
+    return result.Length;
+}
+
+Console.WriteLine(DigitCount(456));
