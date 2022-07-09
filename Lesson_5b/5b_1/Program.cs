bac2aba70b29dbe052eecc0c7e4a1a4fd94fddec
alla <fi-alka@yandex.ru>
2022-07-09 19:31:02 +0300

    Задача 1

---
+++ Lesson_5b/5b_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа замены элементов массива: полож. на отриц. и наоборот.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{       
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);        
+    }    
+    return arr;
+}
+
+void Replace(int[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+Replace(arr_1);
+Print(arr_1);
