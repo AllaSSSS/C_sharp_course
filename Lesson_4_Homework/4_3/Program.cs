50b9c487571c4b130b023e5c5baf9a50231ff45a
alla <fi-alka@yandex.ru>
2022-07-09 14:14:20 +0300

    Д/з 3

---
+++ Lesson_4_Homework/4_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задает массив из 8 элементов случайными числами и выводит их на экран 
+// (использовать FillArray и PrintArray)
+
+void Fill(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] =  new Random().Next();
+    }    
+}
+
+void Print(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < length - 1) Console.Write(", ");
+        else Console.WriteLine();
+    }
+}
+
+int[] array = new int[8];
+
+Fill(array);
+Print(array);
+
