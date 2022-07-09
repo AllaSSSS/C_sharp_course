216173dfcd9cfa109554bf9c69108d37e390cbe5
alla <fi-alka@yandex.ru>
2022-07-09 20:46:21 +0300

    Задача 2

---
+++ Lesson_5b/5b_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// 2. Задайте массив. Напишите программу, которая определяет,
+//    присутствует ли заданное число в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+void NumAvail(int[] arr, int num)
+{
+    string flag = "no";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) 
+        {
+            flag = "yes";
+            break;
+        }
+    }
+    Console.WriteLine(flag);
+}
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+NumAvail(arr_1, 4);
