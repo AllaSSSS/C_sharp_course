bc36c6ab609abe1648495ccb3b3d29591dbe3c0e
alla <fi-alka@yandex.ru>
2022-07-09 23:48:54 +0300

    Д/з №3

---
+++ Lesson_5_Homework/HW_5_3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задать массив вещественных чисел. Найти разницу между максимальным элементом массива.
+
+void Fill(double[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] =  new Random().Next(-100, 100);
+    }    
+}
+
+void Print(double[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < length - 1) Console.Write(", ");
+        else Console.WriteLine();
+    }
+}
+
+double[] array = new double[10];
+Fill(array);
+Print(array);
+
+double DiffMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for(int i = 1; i < array.Length; i++) 
+    {
+        if(array[i] > max) max = array[i];
+        else
+        {
+            if(array[i] < min) min = array[i];
+        }
+    }
+    return max - min;
+}
+
+Console.WriteLine(DiffMaxMin(array));
