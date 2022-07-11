eed2d236ab2c3881ed86df081b0b22b14d95e736
alla <fi-alka@yandex.ru>
2022-07-11 20:20:05 +0300

    Дом. задание №2

---
+++ Lesson_6_Homework/HW_6_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры М чисел. 
+// Посчитать, сколько чисел больше нуля ввел пользователь.
+ 
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введите {M} числа/чисел: ");
+int count = 0;
+
+for (int i = 1; i <= M; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    if (num > 0) count += 1;
+}
+
+Console.WriteLine($"Пользователь ввел {count} числа/чисел больше нуля.");
