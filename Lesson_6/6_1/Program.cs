150b38dc5e5d8bac8241191aee9dbfa785ea7a54
alla <fi-alka@yandex.ru>
2022-07-11 12:48:40 +0300

    демо + 3 задачи с урока

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Существует ли треугольник с такими сторонами.
+
+void Triangle (int numA,int numB, int numC)
+{
+    if(numA + numB > numC && numA + numC > numB && numB + numC > numA)
+    Console.WriteLine("Треугольник существует!");
+    else Console.WriteLine("Нет!");
+}
+
+Triangle(1, 2, 3);
---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Десятичное число в двоичное.
+
+void Binary(int number)
+{
+    string Dec = "";
+    while (number > 0)
+    {
+        Dec = number % 2 + Dec;
+        number /= 2;
+    }
+    Console.WriteLine(Dec);
+}
+
+Binary(45);
+
