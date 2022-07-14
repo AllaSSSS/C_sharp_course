8e22226e3ef2e3aabad2873ca68f8353c3e7bb09
alla <fi-alka@yandex.ru>
2022-07-14 14:58:46 +0300

    Добавила решение с урока с циклом true

--- Lesson_6_Homework/HW_6_2/Program.cs
+++ Lesson_6_Homework/HW_6_2/Program.cs
@@ -1,14 +1,31 @@
 ﻿// Пользователь вводит с клавиатуры М чисел. 
 // Посчитать, сколько чисел больше нуля ввел пользователь.
  
-int M = int.Parse(Console.ReadLine());
-Console.WriteLine($"Введите {M} числа/чисел: ");
-int count = 0;
+int CountNum()
+{ 
+    string num = "";
+    int count = 0;
 
-for (int i = 1; i <= M; i++)
-{
-    int num = int.Parse(Console.ReadLine());
-    if (num > 0) count += 1;
+    while (true)
+    {
+        Console.Write("Введите любое число: ");
+        num = Console.ReadLine();
+
+        if (num == "") return count;
+        else if (int.Parse(num) > 0) count += 1;
+    }
 }
+Console.WriteLine(CountNum());
 
-Console.WriteLine($"Пользователь ввел {count} числа/чисел больше нуля.");
+
+// int count = 0;
+
+// for (int i = 1; i <= M; i++)
+// {
+//     int num = int.Parse(Console.ReadLine());
+//     if (num > 0) count += 1;
+// }
+
+// Console.WriteLine($"Пользователь ввел {count} числа/чисел больше нуля.");
+// int M = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Введите {M} числа/чисел: ");
