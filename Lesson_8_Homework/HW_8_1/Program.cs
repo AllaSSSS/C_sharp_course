b02c18b2f50d7a7616e0516e343570c8138b93ec
alla <fi-alka@yandex.ru>
2022-07-19 12:09:13 +0300

    Задача №1

---
+++ Lesson_8_Homework/HW_8_1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
+// каждой строки массива.
+
+int[,] Fill2DArray(int rows, int columns, int from, int to)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(from, to);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($" {array[i, j],2} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] SortRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            if (array[i, j] < array[i, k])
+            {
+                (array[i, j], array[i, k]) = (array[i, k], array[i, j]);
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = Fill2DArray(5, 6, 1, 100);
+Print2DArray(array);
+
+int[,] sortedArray = SortRows(array);
+Print2DArray(sortedArray);
