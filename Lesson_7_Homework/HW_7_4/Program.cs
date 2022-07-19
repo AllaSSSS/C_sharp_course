3263ce5b360993ddadd103470e18da7378ac7fb2
alla <fi-alka@yandex.ru>
2022-07-19 15:11:33 +0300

    Ввод значений массива вручную

--- Lesson_7_Homework/HW_7_4/Program.cs
+++ Lesson_7_Homework/HW_7_4/Program.cs
@@ -6,13 +6,12 @@ int[,] Fill2DArray(int row, int column)
     int[,] array = new int[row, column];
 
     for (int i = 0; i < row; i++)
-    {   
+    {
         for (int j = 0; j < column; j++)
         {
-            Console.WriteLine($"Какое число находится в {i+1}-ой строке и {j+1}-ом столбце? ");
+            Console.WriteLine($"Какое число находится в {i + 1}-ой строке и {j + 1}-ом столбце? ");
             array[i, j] = int.Parse(Console.ReadLine());
-        }        
-            
+        }
     }
     return array;
 }
@@ -27,26 +26,26 @@ void Print2DArray(int[,] array)
         for (int j = 0; j < columns; j++)
         {
             Console.Write($" {array[i, j]} ");
-        } 
-        Console.WriteLine();       
+        }
+        Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-double[] ColumnAverage (int[,] array)
+double[] ColumnAverage(int[,] array)
 {
     int rows = array.GetLength(0);
     int columns = array.GetLength(1);
     double[] avg = new double[columns];
 
-    for(int j = 0; j < columns; j++)
+    for (int j = 0; j < columns; j++)
     {
         int sum = 0;
-        for(int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++)
         {
             sum += array[i, j];
         }
-        avg[j] = Math.Round((double)sum/rows, 1);
+        avg[j] = Math.Round((double)sum / rows, 1);
     }
     return avg;
 }
@@ -57,11 +56,13 @@ void PrintArray(double[] array)
     for (int i = 0; i < length; i++)
     {
         Console.Write(array[i]);
-        if(i < length - 1) Console.Write("; ");
+        if (i < length - 1) Console.Write("; ");
         else Console.WriteLine();
     }
 }
 
-int[,] array = Fill2DArray(3, 4);
+int[,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+
+// int[,] array = Fill2DArray(3, 4);
 Print2DArray(array);
 PrintArray(ColumnAverage(array));
