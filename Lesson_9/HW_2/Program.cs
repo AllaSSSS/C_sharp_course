5cea30eb4143265f06ecad8cbefa9aae432996c0
alla <fi-alka@yandex.ru>
2022-07-21 23:25:21 +0300

    Д/з №2

---
+++ Lesson_9/HW_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте числа M и N. Напишите программу, которая найдет сумму 
+// натуральных элементов в промежутке от М до N c рекурсией.
+
+
+int SumOfElements(int M, int N)
+{
+    if (N < M) return 0; 
+    else return N + SumOfElements(M, N - 1);
+}
+
+Console.WriteLine(SumOfElements(1, 15));
