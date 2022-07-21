5bba6f884a4b3cb1ce9592eb020546bb276c40ee
alla <fi-alka@yandex.ru>
2022-07-21 10:46:07 +0300

    Задача №1

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Программа, которая выведет все натуральные числа от 1 до N c рекурсией.
+
+void Values (int num)
+{
+    if (num == 0) return;
+    Values (num - 1);
+    Console.Write($"{num}, ");
+}
+
+Values(5);
+
+
