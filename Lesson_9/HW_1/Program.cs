48f7a5320bcdde321ae4dfd9ccd976e0fb2a2d2a
alla <fi-alka@yandex.ru>
2022-07-21 20:50:47 +0300

    Четные числа с рекурсией

---
+++ Lesson_9/HW_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте числа M и N. Напишите программу, которая выведет 
+// все четные натуральные числа в промежутке от М до N c рекурсией.
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+void AllEvens(int M, int N)
+{
+    if (N % 2 == 1) N = N - 1;
+    else if (N < M) return; 
+    else AllEvens(M, N - 2);
+    Console.Write($"{N}, ");
+}
+AllEvens(M, N);
